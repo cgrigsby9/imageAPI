2383e4de846feb634b2ab1447ffff0c82560cd1c
Colin Grigsby <96888510+cgrigsby9@users.noreply.github.com>
2022-02-10 00:38:06 -0600

    Update Controller.ts

--- src/routes/Controller.ts
+++ src/routes/Controller.ts
@@ -50,7 +50,7 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
       res.send('check URL for correct inputs example: fjord&width=100&height=100')
     });
 } catch (err) {
-  res.send('Check URL for correct inputs')
+  res.send('Check URL for correct inputs example: fjord&width=100&height=100')
 }
 res.sendFile(newImage);
 }
